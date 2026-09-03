b3b6e27d882ae29224496e1a025eb54aa10295ce
Nicolás Garrido <nicolasgv58@gmail.com>
2026-09-03 20:18:32 +0000

    Avance 2

--- tarea1.c
+++ tarea1.c
@@ -32,5 +32,43 @@ void registrar_grupo(List *lista) {
     return;
   }
   printf("Ingrese el ID o  Nombre del cliente: ");
-  scanf(" %49[^\n]", ) 
+  scanf(" %49[^\n]", nuevo_grupo->id);
+
+  printf("Ingrese  la cantidad de personas: ");
+  scanf("%d", &nuevo_grupo->cantidad_personas);
+
+  strcpy(nuevo_grupo->prioridad, "Estándar");
+  nuevo_grupo->hora_registro = time(NULL);
+
+  list_pushBack(lista, nuevo_grupo);
+  printf("Grupo registrado exitosamente con prioridad 'Estándar'.\n");
+}
+
+void asignar_prioridad(List *lista) {
+  char id_buscar[50];
+  int opcion_prioridad;
+
+  printf("Ingrese el ID o Nombre del cliente: ");
+  scanf(" %49[^\n]", id_buscar);
+
+  Grupo *grupo_encontrado = NULL;
+  void *current = list_first(lista);
+
+  while (current != NULL) {
+    Grupo *g = (Grupo *)current;
+    if (strcmp(g->id, id_buscar) == 0) {
+      grupo_encontrado = g;
+      break;
+    }
+    current = list_next(lista);
+  }
+  if (grupo_encontrado != NULL) {
+    printf("Seleccione la nueva prioridad:\n");
+    printf("1) VIP\n2) Grupo Grande\n3)Estándar\nOpción: ");
+    scanf("%d", &opcion_prioridad);
+    if (opcion_prioridad == 1) strcpy(grupo_encontrado->prioridad, "VIP");
+    else if (opcion_prioridad == 2) strcpy(grupo_encontrado->prioridad, "Grupo Grande");
+    else if (opcion_prioridad == 3) strcpy(grupo_encontrado->prioridad, "Estándar");
+    else printf("Opción inválida. Se mantiene la prioridad actual.\n");
+  }
 }
